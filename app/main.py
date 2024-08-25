64e232bbf1cc7125f65549c588a502fa09a9d81b
Alexey-Budkovsky <budkovsky.alex@gmail.com>
2024-08-25 15:44:13 +0300

    1.4 #1 Валидация данных с Pydantic

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hotels")
-def get_hotels():
+@app.get("/hotels/{hotel_id}")
+def get_hotels(hotel_id: int):
     return "Отель Бридж Резорт 5 звёзд"
