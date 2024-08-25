8954664b64f6905f7bc95d5decaf93e670d2f5e1
Alexey-Budkovsky <budkovsky.alex@gmail.com>
2024-08-25 18:16:16 +0300

    1.4 #10 Валидация данных с Pydantic

--- app/main.py
+++ app/main.py
@@ -11,17 +11,17 @@ class SHotel(BaseModel):
     address: str
     name: str
     stars: int
+    has_spa: bool
 
 
-@app.get("/hotels", response_model=list[SHotel])
+@app.get("/hotels")
 def get_hotels(
         location: str,
         date_from: date,
         date_to: date,
         has_spa: Optional[bool] = None,
         stars: Optional[int] = Query(None, ge=1, le=5),
-
-):
+)-> list[SHotel]:
     hotels = [
         {
             "address": "Kashtanovaya street 15",
