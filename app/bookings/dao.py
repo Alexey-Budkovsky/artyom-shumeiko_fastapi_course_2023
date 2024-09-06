ac747f0c55d2fcac723b65c08c94221a37d6250c
Alexey-Budkovsky <budkovsky.alex@gmail.com>
2024-09-06 10:39:26 +0300

    1.6 #2 Создание файла `dao.py` в папке `bookings`. Создание папки `dao` и файла `base.py` внутри неё для вынесения логики общих операций с БД в отдельный модуль. Создание метода `find_all` для получения всех данных таблицы.

---
+++ app/bookings/dao.py
@@ -0,0 +1,13 @@
+# data access object --> dao.py
+# -----------------------------
+from app.dao.base import BaseDAO
+from app.database import async_session_maker
+from sqlalchemy import select
+
+from app.bookings.models import Bookings
+
+
+class BookingDAO(BaseDAO):
+    model = Bookings
+
+
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,8 +1,5 @@
 from fastapi import APIRouter
-from sqlalchemy import select
-
-from app.database import async_session_maker
-from app.bookings.models import Bookings
+from app.bookings.dao import BookingDAO
 
 
 router = APIRouter(
@@ -13,12 +10,4 @@ router = APIRouter(
 
 @router.get("")
 async def get_bookings():
-    async with async_session_maker() as session:
-        query = select(Bookings)  # SELECT * FROM bookings
-        result = await session.execute(query)
-        # print(result)
-        # print(result.all())
-        # print(result.scalars().all())
-        # --> Booking.id
-        # --> Booking.total_cost
-        return result.scalars().all()
+    return await BookingDAO.find_all()
